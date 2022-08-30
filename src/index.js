b2531bfc9700f825e8d607722fce96bad3f1fe77
Kingsley Victor <javaprodigy56@gmail.com>
2022-08-30 09:33:52 +0100

    xxxx

--- src/index.js
+++ src/index.js
@@ -9,6 +9,12 @@ app.use(require("morgan")("combined"));
 
 app.use("/api", router);
 
+app.get("/", (req, res) => {
+  return res.status(200).json({
+    msg: "alive"
+  });
+});
+
 app.listen(port, () => {
   db.sequelize.sync().then(() => {
     console.log(`App now running on ${port}`);
